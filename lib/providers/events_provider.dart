da43e8cc92779c3f6ab75d6d107504b6e2bb8255
Jaycee Buarao <jayceebuarao@gmail.com>
2023-12-01 19:57:54 -0800

    create loadEvents() and addEvent() methods

--- lib/providers/events_provider.dart
+++ lib/providers/events_provider.dart
@@ -1,7 +1,9 @@
+import 'package:coucoucountdown/models/event.dart';
+import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
 import 'package:path/path.dart' as path;
-import 'packag';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<Database> _getDatabase() async {
   final dbPath = await sql.getDatabasesPath();
@@ -10,19 +12,55 @@ Future<Database> _getDatabase() async {
     version: 1,
     onCreate: (db, version) {
       return db.execute(
-          'CREATE TABLE user_events(id TEXT PRIMARY KEY, title TEXT, date BLOB, isAllDay INTEGER, hmsTime BLOB, timeUnit TEXT, isCountdown INTEGER, isDone INTEGER, color BLOB, icon BLOB)');
+          'CREATE TABLE user_events(id TEXT PRIMARY KEY, title TEXT, eventDate BLOB, isAllDay INTEGER, hmsTime BLOB, timeUnit TEXT, isCountdown INTEGER, isDone INTEGER, color BLOB, icon BLOB)');
     },
   );
 
   return db;
 }
 
+class UserEventsNotifier extends StateNotifier<List<Event>> {
+  UserEventsNotifier() : super([]);
 
-class UserEventsNotifier extends StateNotifier<> {
-  UserEventsNotifier(): super();
-  
-  Future<void> loadEvents() async{
+  Future<void> loadEvents() async {
     final db = await _getDatabase();
-    final data = await db.query('user_events')
+    final data = await db.query('user_events');
+    final events = data
+        .map(
+          (row) => Event(
+              title: row['title'] as String,
+              eventDate: row['eventDate'] as DateTime,
+              isAllDay: row['isAllDay'] as bool,
+              hmsTime: row['hmsTime'] as DateTime,
+              timeUnit: row['timeUnit'] as CounterUnits,
+              isCountdown: row['isCountdown'] as bool,
+              isDone: row['isDone'] as bool,
+              color: row['color'] as Color,
+              icon: row['icon'] as Icon),
+        )
+        .toList();
+
+    state = events;
+  }
+
+  void addEvent(Event newEvent) async {
+    final db = await _getDatabase();
+    db.insert(
+      'user_events',
+      {
+        'id': newEvent.id,
+        'title': newEvent.title,
+        'eventDate': newEvent.eventDate,
+        'isAllDay': newEvent.isAllDay,
+        'hmsTime': newEvent.hmsTime,
+        'timeUnit': newEvent.timeUnit,
+        'isCountdown': newEvent.isCountdown,
+        'isDone': newEvent.isDone,
+        'color': newEvent.color,
+        'icon': newEvent.icon,
+      },
+    );
+
+    state = [newEvent, ...state];
   }
 }
