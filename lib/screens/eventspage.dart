b9a9fc5722e86fa82c8ca3e1e8d5c6b77c58f954
Jaycee Buarao <jayceebuarao@gmail.com>
2023-12-11 23:23:45 -0800

    build: futurebuilder for events list

--- lib/screens/eventspage.dart
+++ lib/screens/eventspage.dart
@@ -1,5 +1,7 @@
 import 'package:coucoucountdown/providers/events_provider.dart';
 import 'package:coucoucountdown/screens/createeventpage.dart';
+import 'package:coucoucountdown/widgets/gridtile.dart';
+import 'package:coucoucountdown/widgets/listtile.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,6 +15,10 @@ class EventsPage extends ConsumerStatefulWidget {
 
 class _EventsPageState extends ConsumerState<EventsPage> {
   late Future<void> _eventsFuture;
+
+  var isListView = true;
+  var viewIcon = const Icon(Icons.view_list);
+
   @override
   void initState() {
     super.initState();
@@ -21,13 +27,21 @@ class _EventsPageState extends ConsumerState<EventsPage> {
 
   @override
   Widget build(BuildContext context) {
-    // final userEvents = ref.watch(userEventsProvider);
+    final userEvents = ref.watch(userEventsProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Events'),
         centerTitle: true,
-        leading:
-            IconButton(onPressed: () {}, icon: const Icon(Icons.dashboard)),
+        leading: IconButton(
+            onPressed: () {
+              setState(() {
+                isListView = !isListView;
+                isListView
+                    ? viewIcon = const Icon(Icons.view_list_rounded)
+                    : viewIcon = const Icon(Icons.view_cozy_rounded);
+              });
+            },
+            icon: viewIcon),
         actions: [
           IconButton(
               onPressed: () {
@@ -38,7 +52,39 @@ class _EventsPageState extends ConsumerState<EventsPage> {
               icon: const Icon(Icons.add)),
         ],
       ),
-      body: const Text('events will appear here'),
+      body: FutureBuilder(
+          future: _eventsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return isListView == true
+                  ? ListView.separated(
+                      padding: const EdgeInsets.all(16),
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 16),
+                      itemCount: userEvents.length,
+                      itemBuilder: (context, index) {
+                        return EventListTile(event: userEvents[index]);
+                      },
+                    )
+                  : GridView.builder(
+                      padding: const EdgeInsets.all(16),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 0.7,
+                              crossAxisSpacing: 16,
+                              mainAxisSpacing: 16),
+                      itemCount: userEvents.length,
+                      itemBuilder: (context, index) {
+                        return EventGridTile(event: userEvents[index]);
+                      },
+                    );
+            }
+          }),
     );
   }
 }
